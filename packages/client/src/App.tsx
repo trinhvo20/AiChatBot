a3c8593a57d3b7b96e7530a235ef15d0f5d7e726
Trinh Vo <uhttv@sfbcic.com>
2025-12-09 14:19:20 -0600

    Set up Shadcn

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,7 +1,6 @@
-import { use, useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import { Button } from './components/ui/button';
 
 function App() {
   const [message, setMessage] = useState('');
@@ -12,7 +11,13 @@ function App() {
       .then(data => setMessage(data.message));
   }, []);
 
-  return <p className='font-bold'>{message}</p>
+  return (
+    <>
+      <p className='font-bold'>{message}</p>
+      <Button>Click me</Button>
+    </>
+
+  )
 }
 
 export default App
